d781ecef78e1d6f523d955121f46cb0379a891e7
golf128 <baitoey@hotmail.com>
2013-04-23 01:42:36 +0700

    writelcd

---
+++ running_bash/writelcd.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdint.h>
+#include<unistd.h>
+#include<time.h>
+#include<lcd.h>
+#include<wiringPi.h>
+#include"IRremote.c"
+
+int main(void) {
+wiringPiSetup();
+int lcd1;
+char temp[16];
+FILE *fd = NULL;
+fd = fopen("out","r");
+lcd1=lcdInit(2,16,8,11,10,14,13,12,3,2,0,7,9);
+lcdPosition(lcd1,0,0);
+fscanf(fd, "%s", temp);
+lcdPrintf(lcd1,"%s".temp);
+lcdPosition(lcd1,0,1);
+fscanf(fd, "%s", temp);
+lcdPrintf(lcd1,"%s".temp);
+}
